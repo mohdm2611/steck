6e0a3735e91b69eddeb41aa0c982f14161540a9c
Mohammed Mukarram <mohdm2611@gmail.com>
2018-10-13 03:22:46 +0530

    Update message.js

--- message.js
+++ message.js
@@ -1,12 +1,11 @@
-const accountSid = 'AC66dead53c973552acb3620dce467cdb9';
-const authToken = 'c45a57ff20582c121a6bd1a2f2df7673';
+
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
      body: 'The location of the manhole is at maps.google.com',
-     from: '+15312081934',
-     to: '+918686427332'
+     from:
+     to: 
    })
   .then(message => console.log(message.sid))
   .done();
